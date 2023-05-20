50dec3ee49b446b8598d72942a0aed98a0bd0644
dream798 <dream798@live.com>
2023-05-20 20:12:11 +0800

    fix(md5.wasm): 去掉fmt，从2.1减到1.6M

--- md5/md5.go
+++ md5/md5.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"crypto/md5"
-	"fmt"
+// 	"fmt"
 	"syscall/js"
-	"time"
+// 	"time"
+  "encoding/hex"
 )
 
 func main() {
@@ -18,12 +19,12 @@ func registerCallbacks() {
 	/* js.Global().Get("document").
 	Call("getElementById", "brightness").
 	Call("addEventListener", "change", brightnessCb) */
-	js.Global().Set("getFileMd5", js.FuncOf(getFileMd5))
+// 	js.Global().Set("getFileMd5", js.FuncOf(getFileMd5))
 	js.Global().Set("wasmMd5Add", js.FuncOf(wasmMd5Add))
 	js.Global().Set("wasmMd5End", js.FuncOf(wasmMd5End))
 }
 
-func getFileMd5(value js.Value, args []js.Value) interface{} {
+/* func getFileMd5(value js.Value, args []js.Value) interface{} {
 	startTime := time.Now()
 	array := args[0]
 	byteLength := array.Get("byteLength").Int()
@@ -36,7 +37,7 @@ func getFileMd5(value js.Value, args []js.Value) interface{} {
 	elapsed := time.Since(startTime)
 	fmt.Printf("elapsed: %v\n", elapsed)
 	return md5hash
-}
+} */
 
 var md5hash = md5.New()
 
@@ -49,6 +50,9 @@ func wasmMd5Add(value js.Value, args []js.Value) interface{} {
 	return nil
 }
 func wasmMd5End(value js.Value, args []js.Value) interface{} {
-	finalMd5Hash := fmt.Sprintf("%x", md5hash.Sum(nil))
-	return finalMd5Hash
+	/* finalMd5Hash := fmt.Sprintf("%x", md5hash.Sum(nil))
+	return finalMd5Hash */
+
+  finalMd5Hash := hex.EncodeToString(md5hash.Sum(nil))
+  return finalMd5Hash
 }
